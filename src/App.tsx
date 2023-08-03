d4721a667d0492bb689e45afaaef498434293e96
Filipe Sottili <91507688+Sottili@users.noreply.github.com>
2023-08-03 17:37:35 -0300

    Iniciando as ações do formulário e adicionando as tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Css
 import styles from "./App.module.css";
@@ -9,14 +9,23 @@ import Footer from "./components/footer";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 
+//Interface
+import { ITask } from "./interfaces/Task";
+
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+
   return (
     <div>
       <Header />
       <main className={styles.main}>
         <div>
           <h2>Oque você vai fazer?</h2>
-          <TaskForm btnText="Criar Tarefa" />
+          <TaskForm
+            btnText="Criar Tarefa"
+            taskList={taskList}
+            setTaskList={TaskList}
+          />
         </div>
         <div>
           <h2>Suas tarefas:</h2>
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,19 +1,55 @@
-import * as React from "react";
+import React, { useState, ChangeEvent, FormEvent, useEffect } from "react";
 
 //Css
 import styles from "./TaskForm.module.css";
 
+//Interface
+import { ITask } from "../interfaces/Task";
+
 interface Props {
   btnText: string;
+  taskList: ITask[];
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>;
 }
 
-const TaskForm = ({ btnText }: Props) => {
+const TaskForm = ({ btnText, taskList, setTaskList }: Props) => {
+  const [id, setId] = useState<number>(0);
+  const [title, setTitle] = useState<string>("");
+  const [difficulty, setDifficulty] = useState<number>(0);
+
+  const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const id = Math.floor(Math.random() * 1000);
+
+    const newTask: ITask = { id, title, difficulty };
+
+    setTaskList!([...taskList, newTask]);
+
+    setTitle("");
+    setDifficulty(0);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "title") {
+      setTitle(e.target.value);
+    } else {
+      setDifficulty(parseInt(e.target.value));
+    }
+  };
+
   return (
     <>
-      <form className={styles.form}>
+      <form onSubmit={addTaskHandler} className={styles.form}>
         <div className={styles.input_container}>
           <label htmlFor="title">Título: </label>
-          <input type="text" name="title" placeholder="Titulo da tarefa" />
+          <input
+            type="text"
+            name="title"
+            placeholder="Titulo da tarefa"
+            onChange={handleChange}
+            value={title}
+          />
         </div>
         <div className={styles.input_container}>
           <label htmlFor="title">Dificuldade: </label>
@@ -21,6 +57,8 @@ const TaskForm = ({ btnText }: Props) => {
             type="text"
             name="difficulty"
             placeholder="Dificuldade da tarefa"
+            onChange={handleChange}
+            value={difficulty}
           />
         </div>
         <input type="submit" value={btnText} />
